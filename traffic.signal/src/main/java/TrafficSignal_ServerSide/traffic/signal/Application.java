eba86413143a46a83b286d687cf69e373fc26ff4
kk2453 <kartikayshukla3305@gmail.com>
2024-09-01 04:13:26 +0530

    initial commit

---
+++ traffic.signal/src/main/java/TrafficSignal_ServerSide/traffic/signal/Application.java
@@ -0,0 +1,13 @@
+package TrafficSignal_ServerSide.traffic.signal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ traffic.signal/src/test/java/TrafficSignal_ServerSide/traffic/signal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package TrafficSignal_ServerSide.traffic.signal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
