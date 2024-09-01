166bcaa448d51ab7f4cee676165d46d3a45926e7
anshula-lal <151364869+anshula-lal@users.noreply.github.com>
2024-09-01 11:11:48 +0530

    Add files via upload
    ho gya

---
+++ stack.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+class Stack
+{
+private:
+    Node *top;
+    int height;
+
+public:
+    Stack(int value)
+    {
+        Node *newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    void printStack()
+    {
+        Node *temp = top;
+        while (temp)
+        {
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+    void getTop()
+    {
+        cout << "Top: " << top->value << endl;
+    }
+    void getHeight()
+    {
+        cout << "Height: " << height << endl;
+    }
+
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+        height++;
+    }
+
+    int pop(){
+        if(height==0) return  INT_MIN;
+        Node* temp = top;
+        int poppedValue = top->value;
+        top = top->next;
+        delete temp;
+        height--;
+        return poppedValue;
+    }
+};
+
+int main()
+{
+    Stack *myStack = new Stack(2);
+    myStack->push(1);
+    
+    myStack->printStack();
+}
